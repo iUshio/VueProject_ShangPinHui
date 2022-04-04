709a02a55494d3638b91d4f00206827cce4a1234
iUshio <97095966+iUshio@users.noreply.github.com>
2022-04-04 18:23:55 +0800

    shopChart

--- app/src/store/shopCart/index.js
+++ app/src/store/shopCart/index.js
@@ -1,18 +1,29 @@
 import { reqCartList } from '@/api'
 
 const state = {
+    cartList:[],
 }
 const mutations = {
-
+    GETCARTLIST(state,cartList){
+        state.cartList = cartList
+    }
 }
 const actions = {
     // 获取购物车列表的数据
     async getCartList({commit}){
         let result = await reqCartList();
-        console.log(result);
+        if(result.code == 200){
+            commit('GETCARTLIST',result.data)
+        }
     }
 }
 const getters = {
+    cartList(state){
+        return state.cartList[0] || {};
+    },
+    // cartInfoList(state){
+    //     return state.cartList
+    // }
 }
 
 export default {
